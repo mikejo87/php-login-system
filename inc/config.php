ec42d7c65420de58749b19c1777ad3fd2d2ccb60
unknown <michael.jovic87@gmail.com>
2019-12-09 20:24:50 +1030

    added first ajax page

--- inc/config.php
+++ inc/config.php
@@ -2,10 +2,12 @@
 	//if there is no constant defined called __CONFIG__ do not load this file
 	if(!defined('__CONFIG__')) {
 		exit('You do not have a config file');
-		// 
 	}
 
-	define('ALLOW_FOOTER', true);
+	//define('ALLOW_FOOTER', true);
 	//our config is below
+	include_once "classes/DB.php";
+
+	$con = DB::getConnection();
 
 ?>
--- index.php
+++ index.php
@@ -1,41 +1,38 @@
 <?php 
-	//allow the config
+
+	// Allow the config
 	define('__CONFIG__', true);
-	//require the config
-	require_once "inc/config.php"; ?>
+	// Require the config
+	require_once "inc/config.php"; 
 
+?>
 <!DOCTYPE html>
-<html lang="en">	
-	<head>
-		<meta charset="utf-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width,  initial-scale=1" />
-		<meta name="robots" content="follow">
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-		<title>Page Title</title>
+    <title>Page Title</title>
 
-		<base href="/" />
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
-	</head>
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
 
-	<body>
+  <body>
 
+  	<div class="uk-section uk-container">
+  		<?php 
+  			echo "Hello world. Today is: ";
+  			echo date("Y m d");
+  		?> 
+  		<p>
+  			<a href="php_login_system/login.php">Login</a>
+  			<a href="php_login_system/register.php">Register</a>
+  		</p>
+  	</div>
 
-		<div class="uk-section uk-container">
-		<?php
-			echo "hello world today is: ";
-			echo date("Y m d");
-			?>
-			<p>
-				<a href="php_login_system/login.php">Login</a>
-				<a href="php_login_system/register.php">Register</a>
-			</p>
-	</div>
-
-	<?php require_once "inc/footer.php"; ?>
-		
-
-
-		
-	</body>
-	</html>
+  	<?php require_once "inc/footer.php"; ?> 
+  </body>
+</html>
